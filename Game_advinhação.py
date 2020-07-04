df3aed9e4680f8682171d07659ab25e6abdba98c
4ndr3lg <66439412+4ndr3lg@users.noreply.github.com>
2020-07-04 01:14:07 -0300

    Add files via upload

---
+++ Game_advinhação.py
@@ -0,0 +1,12 @@
+print("***********************************")
+print ("Bem vindos ao jogo de advinhação!")
+print("**********************************")
+Numero_secreto ="1859"
+chute= input("Digite o seu número: ")
+print("Você digitou: ", chute)
+if (Numero_secreto == chute):
+     print("Você acertou!")
+else:
+    print("Você errou!")
+
+
